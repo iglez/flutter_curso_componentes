d2dbebe7ac0bf9494063820f5ed1af38e5a808de
Ivan Gonzalez <ivan@iconos.mx>
2021-02-08 19:03:31 -0800

    Creando la pagina de cards y direccionandola en rutas

---
+++ componentes/lib/src/pages/card_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(title: Text('Cards'),),
+      ),
+    );
+  }
+}
