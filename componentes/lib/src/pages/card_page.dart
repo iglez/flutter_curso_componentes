e42d0b95228805a29c75ad346efee6d5f1f14abc
Ivan Gonzalez <ivan@iconos.mx>
2021-02-08 19:16:29 -0800

    Creando nuestra tarjeta

--- componentes/lib/src/pages/card_page.dart
+++ componentes/lib/src/pages/card_page.dart
@@ -7,6 +7,37 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards'),
       ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: [_cargarTipo1()]
+      ),
+    );
+  }
+
+  Widget _cargarTipo1() {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue,),
+            title: Text('Soy el titulo'),
+            subtitle: Text('Aqui estamos en la descripcion de la tarjeta viendo la idea de lo que quiero mostrarles con mucho text'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FlatButton(
+                onPressed: () {},
+                child: Text('Cancelar')
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text('Ok')
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
