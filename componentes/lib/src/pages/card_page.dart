bef3cac2d095f5d84304910cf70654bfd7f123fb
Ivan Gonzalez <ivan@iconos.mx>
2021-02-08 19:06:02 -0800

    Removiendo el container que tenia de mas

--- componentes/lib/src/pages/card_page.dart
+++ componentes/lib/src/pages/card_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CardPage extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(title: Text('Cards'),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
       ),
     );
   }
