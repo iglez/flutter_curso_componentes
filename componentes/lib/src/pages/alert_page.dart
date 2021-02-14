bc7bc1121f54df65485f950eb64f70441770b0a0
Ivan Gonzalez <ivan@iconos.mx>
2021-02-14 11:51:17 -0800

    Botonsitos para cerrar

--- componentes/lib/src/pages/alert_page.dart
+++ componentes/lib/src/pages/alert_page.dart
@@ -29,20 +29,31 @@ class AlertPage extends StatelessWidget {
 
   void mostrarAlerta(BuildContext context) {
     showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (context) {
-        return AlertDialog(
-          title: Text('Titulo'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Este es el contenido de la caja de la alerta'),
-              FlutterLogo( size: 100.0, )
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Este es el contenido de la caja de la alerta'),
+                FlutterLogo(
+                  size: 100.0,
+                )
+              ],
+            ),
+            actions: [
+              FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text('Cancelar')),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('OK'))
             ],
-          ),
-        );
-      }
-    );
+          );
+        });
   }
 }
