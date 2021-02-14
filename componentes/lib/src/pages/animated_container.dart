05f16fc1345a58c6c323308464828b44a4119f44
Ivan Gonzalez <ivan@iconos.mx>
2021-02-14 12:20:15 -0800

    OPS se convirtio en statefull, y al dar click en boton se cambio propiedad

--- componentes/lib/src/pages/animated_container.dart
+++ componentes/lib/src/pages/animated_container.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
-class AnimatedContainerPage extends StatelessWidget {
+class AnimatedContainerPage extends StatefulWidget {
   // const AnimatedContainerPage({Key key}) : super(key: key);
 
+  @override
+  _AnimatedContainerPageState createState() => _AnimatedContainerPageState();
+}
+
+class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
   double _width = 50.0;
+
   double _height = 50.0;
+
   Color _color = Colors.pink;
 
   BorderRadiusGeometry _borderRadius = BorderRadius.circular(8.0);
@@ -12,18 +19,27 @@ class AnimatedContainerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Animated Container'),
+      appBar: AppBar(
+        title: Text('Animated Container'),
+      ),
+      body: Center(
+        child: Container(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(borderRadius: _borderRadius, color: _color),
         ),
-        body: Center(
-          child: Container(
-            width: _width,
-            height: _height,
-            decoration: BoxDecoration(
-              borderRadius: _borderRadius,
-              color: _color
-            ),
-          ),
-        ));
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_arrow),
+        onPressed: cambiarForma,
+      ),
+    );
+  }
+
+  void cambiarForma() {
+    _width += 50.0;
+    _height += 50.0;
+    _color = Colors.red;
+    setState(() {});
   }
 }
