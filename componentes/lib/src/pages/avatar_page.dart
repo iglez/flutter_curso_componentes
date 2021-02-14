a433fb00ab960f30842e82e5139b42650779d3ee
Ivan Gonzalez <ivan@iconos.mx>
2021-02-14 12:00:19 -0800

    Agregando avatar con imagen

--- componentes/lib/src/pages/avatar_page.dart
+++ componentes/lib/src/pages/avatar_page.dart
@@ -8,6 +8,13 @@ class AvatarPage extends StatelessWidget {
         title: Text('Avatar Page'),
         actions: [
           Container(
+            padding: EdgeInsets.all(3.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://media.indiedb.com/images/members/1/489/488697/mandoavatar.1.jpg'),
+              radius: 20.0,
+            ),
+          ),
+          Container(
             margin: EdgeInsets.only(right: 10.0),
             child: CircleAvatar(
               child: Text('IG'),
