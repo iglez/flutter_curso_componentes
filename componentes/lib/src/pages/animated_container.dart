5a0304338bce52eeadbd1af0bef70a4ee517d859
Ivan Gonzalez <ivan@iconos.mx>
2021-02-14 12:15:56 -0800

    Creando scaffold y contenedor principal

--- componentes/lib/src/pages/animated_container.dart
+++ componentes/lib/src/pages/animated_container.dart
@@ -3,10 +3,27 @@ import 'package:flutter/material.dart';
 class AnimatedContainerPage extends StatelessWidget {
   // const AnimatedContainerPage({Key key}) : super(key: key);
 
+  double _width = 50.0;
+  double _height = 50.0;
+  Color _color = Colors.pink;
+
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8.0);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Container'),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Animated Container'),
+        ),
+        body: Center(
+          child: Container(
+            width: _width,
+            height: _height,
+            decoration: BoxDecoration(
+              borderRadius: _borderRadius,
+              color: _color
+            ),
+          ),
+        ));
   }
 }
