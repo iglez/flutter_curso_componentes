00ffccb0cc4c35edd81c6173283dab78bbf2384e
Ivan Gonzalez <ivan@iconos.mx>
2021-02-20 16:04:45 -0800

    Desplegando imagenes por indice

--- componentes/lib/src/pages/listview_page.dart
+++ componentes/lib/src/pages/listview_page.dart
@@ -8,13 +8,31 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
+  List<int> _enteros = [1, 2, 3, 4, 5];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Listas'),
       ),
-      body: Text('Listas page'),
+      body: _crearLista(),
     );
+  }
+
+  Widget _crearLista() {
+    // https://picsum.photos/
+
+    return ListView.builder(
+        itemCount: _enteros.length,
+        itemBuilder: (BuildContext context, int index) {
+
+          final imagen = _enteros[index];
+
+          return FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage('https://picsum.photos/200/300?random=$imagen'),
+          );
+        });
   }
 }
